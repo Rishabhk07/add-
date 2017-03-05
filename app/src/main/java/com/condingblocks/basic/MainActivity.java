7cd760341dff91a0d2390309a8bb31a2a6089dd6
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-03-05 11:11:06 +0530

    initial commit
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/com/condingblocks/basic/MainActivity.java
@@ -0,0 +1,93 @@
+package com.condingblocks.basic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "Main Activity";
+
+    EditText etNum1;
+    EditText etNum2;
+    Button btnAdd;
+    TextView tvResult;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etnum2);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        tvResult = (TextView) findViewById(R.id.result);
+
+        View.OnClickListener ocl =  new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if(R.id.btnAdd == view.getId()){
+                    addandShow();
+                }
+
+            }
+        };
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
+        Log.v(TAG , "onCreate: ");
+    }
+
+    void addandShow(){
+        int a = Integer.parseInt(etNum1.getText().toString());
+        int b = Integer.parseInt(etNum2.getText().toString());
+
+        tvResult.setText(String.valueOf(a + b));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+}
